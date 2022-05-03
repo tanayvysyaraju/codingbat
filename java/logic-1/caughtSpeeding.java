e392adbfd67affb962a00ebddadbcdf4e95d5613
tanayvysyaraju <95253809+tanayvysyaraju@users.noreply.github.com>
2022-05-03 10:00:24 -0400

    Update caughtSpeeding.java

--- java/logic-1/caughtSpeeding.java
+++ java/logic-1/caughtSpeeding.java
@@ -9,7 +9,7 @@ public int caughtSpeeding(int speed, boolean isBirthday) {
     if(isBirthday) {
         if(speed <= 65) {
             return 0;
-        } else if(66 <= speed && speed <= 85) {
+        } else if(66 >= speed && speed <= 85) {
             return 1;
         } else if(86 <=  speed) {
             return 2;
@@ -18,7 +18,7 @@ public int caughtSpeeding(int speed, boolean isBirthday) {
                                         
     if(speed <= 60) {
         return 0;
-    } else if(61 <= speed && speed <= 80) {
+    } else if(61 >= speed && speed <= 80) {
         return 1;
     } else {
         return 2;
